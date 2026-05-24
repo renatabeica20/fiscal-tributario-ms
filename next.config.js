049ee8975467a202967b89eb587b8f092b3dba02
renatabeica20 <renata.beica20@gmail.com>
2026-05-24 13:01:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
