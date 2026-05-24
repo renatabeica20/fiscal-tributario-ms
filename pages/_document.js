6f74ef08f9041d5a57579904f0722d2a30cb1063
renatabeica20 <renata.beica20@gmail.com>
2026-05-24 01:34:15 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#0d2f5e" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Oráculo Fiscal MS" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
